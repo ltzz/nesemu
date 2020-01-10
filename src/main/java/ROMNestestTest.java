bb8a59b14e399de73f3422dbb97e585e5ca22e46
ltzz <ltzz@users.noreply.github.com>
2020-01-11 01:15:46 +0900

    feat nestest.nesの確認用クラス追加
    Signed-off-by: ltzz <ltzz@users.noreply.github.com>

---
+++ src/main/java/ROMNestestTest.java
@@ -0,0 +1,30 @@
+import system.NESSystem;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public final class ROMNestestTest {
+
+    public static void run(){
+        try {
+            NESSystem system = new NESSystem("./nestest.nes");
+            File logfile = new File("./nestest.log");
+            FileReader fileReader = new FileReader(logfile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            system.cpu.programCounter = 0xC000;
+            for (int i = 0; i < 1000; ++i) {
+                String logProgramCounter = bufferedReader.readLine().split("\\s")[0];
+                String nowProgramCounter = String.format("%04X", system.cpu.programCounter);
+                if(!logProgramCounter.equals(nowProgramCounter)){
+                    System.out.println((i+1) + " " + nowProgramCounter
+                            + " A:" + String.format("%02X", system.cpu.getRegA())
+                            + " P:" + String.format("%02X", system.cpu.getRegP()));
+                }
+                system.systemExecute();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
